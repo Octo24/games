1930feb9b5160024dfb3f2dcbce9486ec412609a
Octo24 <octavian.hagi-memet@edu.sait.ca>
2023-10-29 21:46:00 -0600

    Commit 3: create snake class

--- snake.py
+++ snake.py
@@ -15,10 +15,31 @@ FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR ="#000000"
 
 class Snake:
-    pass
+    
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
 
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
+        
+
+# defines how food moves on gameboard
 class Food:
-    pass
+    
+    def __init__(self):
+
+        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE)-1) * SPACE_SIZE
+        
+        self.coordinates = [x, y]
+
+        canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tags="food")
 
 def next_turn():
     pass
@@ -47,5 +68,22 @@ label.pack()
 # create background (score and background)
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
+
+# centers window
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2) - (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+# making snake 
+snake = Snake()
+food = Food()
 
 window.mainloop()
