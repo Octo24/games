2ae6dd072813d2b7d251c060de3150977434414f
Octo24 <octavian.hagi-memet@edu.sait.ca>
2023-10-29 21:00:36 -0600

    Initial Commit: set classes, functions & constants

---
+++ snake.py
@@ -0,0 +1,40 @@
+# import python GUI from tkinter, import all with *
+from tkinter import *
+
+# self explanatory
+import random
+
+# constants (uppercase convention)
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR ="#000000"
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def game_over():
+    pass
+
+# game window 
+window = Tk()
+window.title("Snake game")
+window.resizable(False, False)
+
+window.mainloop()
