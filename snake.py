4b7c5f24961633f707125cd74e2daaf7378f5019
Octo24 <octavian.hagi-memet@edu.sait.ca>
2023-10-29 22:04:08 -0600

    Commit 5: Change direction class

--- snake.py
+++ snake.py
@@ -61,11 +61,33 @@ def next_turn(snake, food):
 
     snake.squares.insert(0, square)
 
+    del snake.coordinates[-1]
+
+    canvas.delete(snake.squares[-1])
+
+    del snake.squares[-1]
+
     window.after(SPEED, next_turn, snake, food)
     
 
 def change_direction(new_direction):
-    pass
+    
+    global direction
+    # to avoid 180 turn
+    if new_direction == 'left':
+        if direction != 'right':
+            direction = new_direction
+    elif new_direction == 'right':
+        if direction != 'left':
+            direction = new_direction
+    elif new_direction == 'up':
+        if direction != 'down':
+            direction = new_direction
+    elif new_direction == 'down':
+        if direction != 'up':
+            direction = new_direction
+        
+
 
 def check_collisions():
     pass
@@ -101,6 +123,13 @@ x = int((screen_width/2) - (window_width/2))
 y = int((screen_height/2) - (window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+# binding keys
+window.bind('<Left>', lambda event: change_direction('left'))
+window.bind('<Right>', lambda event: change_direction('right'))
+window.bind('<Up>', lambda event: change_direction('up'))
+window.bind('<Down>', lambda event: change_direction('down'))
+
 
 # making snake 
 snake = Snake()
